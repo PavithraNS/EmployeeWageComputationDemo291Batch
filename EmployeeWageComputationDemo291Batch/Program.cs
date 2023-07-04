7dd0321a6f928b3730cb4e3135e8c6b550c23704
PavithraNS <nspavi1991@gmail.com>
2023-07-04 08:44:07 +0530

    [Pavithra] modifying program.cs file to find employee's attendence

--- EmployeeWageComputationDemo291Batch/Program.cs
+++ EmployeeWageComputationDemo291Batch/Program.cs
@@ -10,7 +10,20 @@ namespace EmployeeWageComputationDemo291Batch
     {
         static void Main(string[] args)
         {
+            const int IS_PRESENT = 1;
             Console.WriteLine("Welcome to EmployeeWageComputation");
+            //UC-1 finding employee attendence 
+            Random random = new Random();
+            int employeeInput=random.Next(0,2); //0 or 1
+            Console.WriteLine("Random value:{0}", employeeInput);
+            if (employeeInput==IS_PRESENT)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
             Console.ReadLine();
         }
     }
